fca4caa47bdecdbec3b0836bf5a4673d7fd12405
Naren <palepnaren@gmail.com>
2017-10-22 16:17:40 -0500

    fixed bug in email

--- public/javascript/profile.js
+++ public/javascript/profile.js
@@ -122,7 +122,7 @@ $(function(){
     $('#email-form').on("submit",function(e){
         alert("email sent successfully");
         window.setTimeout(function(){
-            window.location.assign("https://narentest.herokuapp.com/");
+            window.location.assign("https://narentest.herokuapp.com");
         },100);
     });
 
