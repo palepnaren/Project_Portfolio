012d872db6ba33e5c689ba2215fe4260965602ad
Naren <palepnaren@gmail.com>
2017-10-27 17:31:50 -0500

    final release of my site

--- public/javascript/guage.js
+++ public/javascript/guage.js
@@ -6,5 +6,6 @@ $('svg').each(function(i,elm){
     config.waveHeight=0.2;
     config.circleColor="#222";
     var guage = loadLiquidFillGauge($(elm).attr('id'), $(elm).data('percentage'),config); 
+
 });
 
